55d6eee271f4ab8e2981cd369ae6ac04ee2fc1dc
Arpit-Shinde <arpitshinde9@gmail.com>
2026-07-29 20:07:50 +0530

    reverting back to stable version

---
+++ build/CMakeFiles/Collision_Sim.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Collision_Sim.dir/app/main.cpp.obj"
+  "CMakeFiles/Collision_Sim.dir/app/main.cpp.obj.d"
+  "CMakeFiles/Collision_Sim.dir/src/engine.cpp.obj"
+  "CMakeFiles/Collision_Sim.dir/src/engine.cpp.obj.d"
+  "CMakeFiles/Collision_Sim.dir/src/geometry.cpp.obj"
+  "CMakeFiles/Collision_Sim.dir/src/geometry.cpp.obj.d"
+  "CMakeFiles/Collision_Sim.dir/src/glad.c.obj"
+  "CMakeFiles/Collision_Sim.dir/src/glad.c.obj.d"
+  "CMakeFiles/Collision_Sim.dir/src/shader.cpp.obj"
+  "CMakeFiles/Collision_Sim.dir/src/shader.cpp.obj.d"
+  "Collision_Sim.exe"
+  "Collision_Sim.exe.manifest"
+  "Collision_Sim.pdb"
+  "libCollision_Sim.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Collision_Sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
